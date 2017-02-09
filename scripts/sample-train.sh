766c4cf3193321ece9063a03c0d3536fd075ebbb
kamigaito <kamigaito@lr.pi.titech.ac.jp>
2017-02-10 00:20:38 +0900

    sample train file was modified.

--- scripts/sample-train.sh
+++ scripts/sample-train.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 ./bin/seq2seq \
---rootdir ./sample/model
---srcfile ./sample/src.ja
---trgfile ./sample/trg.en
---srcvalfile ./sample/srcval.ja
+--mode train \
+--rootdir ./sample/model \
+--srcfile ./sample/src.ja \
+--trgfile ./sample/trg.en \
+--srcvalfile ./sample/srcval.ja \
 --trgvalfile ./sample/trgval.en
